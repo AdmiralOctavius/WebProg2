3514547a37c01d2ef3fd1ecd555109bdff9bb644
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-01-23 10:30:54 -0500

    Created bootstrap, next class
    Did chapter 2 in class.

--- Learning-jQuery-3-master/Chapter02/02.js
+++ Learning-jQuery-3-master/Chapter02/02.js
@@ -4,3 +4,51 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(() => {
+$('#selected-plays > li')
+    .addClass('horizontal');
+$('#selected-plays li:not(.horizontal)')
+    .addClass('sub-level');
+$('#selected-plays li:not(.sub-level)')
+    .addClass('big-letter');
+
+//Copy Pasted in
+$('a[href^="mailto:"]')
+    .addClass('mailto');
+$('a[href$=".pdf"]')
+    .addClass('pdflink');
+$('a[href^="http"][href*="henry"]')
+    .addClass('henrylink');
+
+//$('tr:even').addClass('alt');
+//$('tr:nth-child(odd)').addClass('alt');
+$('tr')
+    .filter(':even')
+    .addClass('alt');
+
+$('td:contains(Henry)')
+    .next()
+    .addClass('highlight');
+
+$('a')
+		.filter((i,	a)	=> a.hostname	&&	a.hostname	!==	location.hostname)
+				.addClass('external');
+
+$('td:contains(Henry)')
+	 .parent()
+   .children()
+   .addClass('highlight');
+
+const	forText	=	[];
+	 for	(let	td	of	$('td'))	{
+			if	(td.textContent.startsWith('H'))	{
+				forText.push(td.textContent);
+			}
+}
+	 console.log('for',	forText);
+
+
+console.log($('.highlight').get(0).tagName);
+
+
+});
