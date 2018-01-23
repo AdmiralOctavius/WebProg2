5e0e2b716af78266b5420898e7bab30347ed44c3
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-01-23 10:43:31 -0500

    Started chapter 3

--- Learning-jQuery-3-master/Chapter03/03.js
+++ Learning-jQuery-3-master/Chapter03/03.js
@@ -4,3 +4,23 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(()	=>	{
+		$('#switcher-default')
+				.on('click',	()	=>	{
+						$('body')
+								.removeClass('narrow')
+								.removeClass('large');
+		});
+		$('#switcher-narrow')
+				.on('click',	()	=>	{
+						$('body')
+								.addClass('narrow')
+								.removeClass('large');
+    });
+		$('#switcher-large')
+				.on('click',	()	=>	{
+						$('body')
+								.removeClass('narrow')
+								.addClass('large');
+    });	
+});
