4d1f14ad00251ba3d6e84d660cfb8f5a13b1f50d
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-01-30 10:21:32 -0500

    Class 1/30
    Changed a whole lot, implemented the php on the server end. Make sure to log in with marshall/bennett253

--- Learning-jQuery-3-master/Chapter04/listings/4.1.js
+++ Learning-jQuery-3-master/Chapter04/listings/4.1.js
@@ -1,6 +1,20 @@
 $(() => {
-  $('#switcher-large')
-    .click(() => {
+  const	$speech	=	$('div.speech');
+  const defaultSize = parseFloat($speech.css('fontSize'));
+	$('#switcher-large')
+		.click(()	=>	{
+				const	num	=	parseFloat($speech.css('fontSize'));
+				$speech.css('fontSize',	`${num	*	1.4}px`);
+	});
+  $('#switcher-small')
+		.click(()	=>	{
+				const	num	=	parseFloat($speech.css('fontSize'));
+				$speech.css('fontSize',	`${num	/	1.4}px`);
+	});
 
-    });
+  $('#switcher-default')
+    .click(()	=>	{
+        const	num	=	parseFloat($speech.css('fontSize'));
+        $speech.css('fontSize',	`${defaultSize}px`);
+  });
 });
