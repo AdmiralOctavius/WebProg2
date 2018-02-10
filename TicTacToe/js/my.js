4c7ab97dba8fe00930a6e1fdfd72275c796d24de
AdmiralOct <buildgamessnotbombs@gmail.com>
2018-02-10 14:47:55 -0500

    FIXED IT FUUU

--- TicTacToe/js/my.js
+++ TicTacToe/js/my.js
@@ -8,13 +8,13 @@ $(() => {
     $(".box").click( function(){
         if(turn1==false && !($(this).hasClass("O") || $(this).hasClass("X") )){
           $(this).addClass("O");
-          $(this).append("<img class='selected' src='images/O.png'>");
+          $(this).append("<img class='ticTacToeImage' src='images/O.png'>");
           turn1=true;
           grid[0] = 1;
         }
         else if(turn1==true && !($(this).hasClass("O") || $(this).hasClass("X") )){
           $(this).addClass("X");
-          $(this).append("<img class='selected' src='images/X.png'>");
+          $(this).append("<img class='ticTacToeImage' src='images/X.png'>");
           turn1=false;
           grid[0] = 2;
         }
