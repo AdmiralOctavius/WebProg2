2844372757b6b83f85765339317440875eb4938c
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-02-21 10:47:30 -0500

    JQuery 2, found Weather api

--- WeatherAPI/js/my.js
+++ WeatherAPI/js/my.js
@@ -7,6 +7,9 @@ $(() => {
       $( "#results" ).empty();
       jQuery.each( zip  , function( i, field ) {
           $( "#results" ).append( zip[0].value + " " );
+
+      //$.getJSON("api.openweathermap.org/data/2.5/weather?zip={" + zip[0].value + "},{us}&APPID={e683b50e91c6b7372e75995b3058f380}", function(){ console.log("Sent Data");});
+      $.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={05cecf912641e99ee3eb3c056de867ab}", function(){ console.log("Sent Data");});
     });
     });
 
@@ -15,15 +18,16 @@ $(() => {
 
 function validateForm(){
     var x = document .forms["myForm"]["Zipcode"].value;
+    x = parseInt(x);
     if(x==""){
       alert("Need a valid zip");
       return false;
     }
-    if(x>5){
+    if(x.length>5){
       alert("Need a valid zip");
       return false;
     }
-    if(x<5){
+    if(x.length<5){
       alert("Need a valid zip");
       return false;
     }
