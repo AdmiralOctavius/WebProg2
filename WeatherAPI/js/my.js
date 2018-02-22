9bc3e8ec825a3ba22bc0bef15f498b21d2d3f384
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-02-22 10:44:34 -0500

    Worked on Assign 2. GOT API WORKING!!

--- WeatherAPI/js/my.js
+++ WeatherAPI/js/my.js
@@ -9,7 +9,14 @@ $(() => {
           $( "#results" ).append( zip[0].value + " " );
 
       //$.getJSON("api.openweathermap.org/data/2.5/weather?zip={" + zip[0].value + "},{us}&APPID={e683b50e91c6b7372e75995b3058f380}", function(){ console.log("Sent Data");});
-      $.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={05cecf912641e99ee3eb3c056de867ab}", function(){ console.log("Sent Data");});
+      //$.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={05cecf912641e99ee3eb3c056de867ab}", function(){ console.log("Sent Data");});
+      //$('#results').load("http://api.openweathermap.org/data/2.5/weather?zip=26035,{us},&APPID={e683b50e91c6b7372e75995b3058f380}");
+      $.get("http://api.openweathermap.org/data/2.5/weather?zip=26035,us&APPID=05cecf912641e99ee3eb3c056de867ab", function(data){
+          $("#results").after(data.name);
+          $("#results").after(data.coord.lat, data.coord.lon)
+          $("#results").after('<br',data.main.temp, data.main.pressure, data.main.humidity);
+      });
+      //$("#results").append("<a href=api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=05cecf912641e99ee3eb3c056de867ab'>Test</a>");
     });
     });
 
