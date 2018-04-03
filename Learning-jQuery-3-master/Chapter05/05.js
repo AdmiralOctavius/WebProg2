c60e12cffc6d2c7c16867080dd0a0744247ea851
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-04-03 09:37:32 -0400

    Old Jquery

--- Learning-jQuery-3-master/Chapter05/05.js
+++ Learning-jQuery-3-master/Chapter05/05.js
@@ -1,6 +1,32 @@
-// This is the custom JavaScript file referenced by index.html. You will notice
-// that this file is currently empty. By adding code to this empty file and
-// then viewing index.html in a browser, you can experiment with the example
-// page or follow along with the examples in the book.
-//
-// See README.txt for more information.
+$(()	=>	{
+    $('p').text(function(){
+      return $(this).text().replace("Circle", "Square");
+    })
+    const	$notes	=	$('<ol	id="notes"></ol>')
+        .insertBefore('#footer');
+
+    $('div.chapter	p:eq(0)')
+			.clone()
+    .insertBefore('div.chapter');
+
+    $('span.footnote')
+.each((i,	span)	=>	{
+$(span)
+  .before([
+      '<a	href="#footnote-',
+      i	+	1,
+      '"	id="context-',
+      i	+	1,
+      '"	class="context">',
+      '<sup>',
+      i	+	1,
+      '</sup></a>'
+  ].join(''))
+  .appendTo($notes)
+  .append([
+      '&nbsp;(<a	href="#context-',
+      i	+	1,
+      '">context</a>)'
+].join(''))
+.wrap('<li></li>');
+});	});
